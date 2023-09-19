94d7ee4a0b19e579f130999401429999f5e59fe5
ljm-cloud <83705498@qq.com>
2023-09-19 15:21:19 +0800

    separation method

--- src/main/java/com/example/demo/metries/MeterService.java
+++ src/main/java/com/example/demo/metries/MeterService.java
@@ -1,11 +1,14 @@
 package com.example.demo.metries;
 
 import com.example.demo.helper.ThreadPoolHelper;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.Timer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,16 +27,52 @@ public class MeterService {
 
     @Autowired
     private MeterRegistry meterRegistry;
-
-    private AtomicLong sessionCount = new AtomicLong(0);
-
     private Integer integer = 0;
     public List<String> list = new ArrayList<>();
     List<Integer> integers;
-
     private Map<String,String> map;
-
     private final AtomicInteger atomicInteger = new AtomicInteger();
+    public CompletableFuture<Void> counter(){
+        return CompletableFuture.runAsync(()->{
+            while (true){
+                Counter counter = meterRegistry.counter("counter_increment");
+                counter.increment();
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public CompletableFuture<Void> gauge(){
+        return CompletableFuture.runAsync(()->{
+            meterRegistry.gauge("list_size_gauge", Collections.emptyList(),list, List::size);
+            integers = meterRegistry.gaugeCollectionSize("collection_size_gauge", Tags.empty(), Lists.newArrayList());
+            map = Maps.newHashMap();
+            meterRegistry.gaugeMapSize("map_size_gauge",Tags.empty(),map);
+            meterRegistry.gauge("atomic_integer_gauge",atomicInteger);
+            while (true){
+                list.add("00");
+                integers.add(1);
+                map.put("aa","aa");
+                int i = atomicInteger.getAndIncrement();
+                if (i%30==0){
+                    //每增加到30就归0
+                    atomicInteger.set(0);
+                }
+                //测试每次传Integer数，gauge会不会变化
+                meterRegistry.gauge("random_int_gauge", RandomUtils.nextInt(1000));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
     public CompletableFuture<Void> addCounter(){
         meterRegistry.gauge("list_size_gauge", Collections.emptyList(),list, List::size);
         integers = meterRegistry.gaugeCollectionSize("collection_size_gauge", Tags.empty(),new ArrayList<Integer>());
